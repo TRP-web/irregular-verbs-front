99a591f5995646b2f147694b240585a1c1de3edd
unknown <kusokpocki@gmail.com>
2022-12-29 18:20:46 +0300

    relize 2.0 test bug on versel

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -9,9 +9,9 @@ const App = ({ Component, pageProps }) => {
     return (
         <Provider store={setupStore()}>
             <GoogleOAuthProvider clientId='297750495683-im2hja8uvhhopfjp3rl0g0plkdplvoqf.apps.googleusercontent.com'>
-                {/* <Header> */}
-                <Component {...pageProps} />
-                {/* </Header> */}
+                <Header>
+                    <Component {...pageProps} />
+                </Header>
             </GoogleOAuthProvider>
         </Provider>
     )
